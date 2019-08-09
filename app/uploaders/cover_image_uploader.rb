6d7e5a0410fbba270bc25b26fe3745ff9dd116db
Elliot Mousseau <supercontraption@gmail.com>
2019-08-09 11:52:11 -0400

    heroku pg fix attempt 8 - local img storage

--- app/uploaders/cover_image_uploader.rb
+++ app/uploaders/cover_image_uploader.rb
@@ -7,11 +7,11 @@ class CoverImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [1024, 1024]
   
-  if Rails.env.production?
-    storage :fog
-  else
+ #if Rails.env.production?
+  #  storage :fog
+  #else
     storage :file
-  end
+  #end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
