9419a3426e429a1c6a23024a049354ab304717e5
Elliot Mousseau <supercontraption@gmail.com>
2019-08-09 09:36:45 -0400

    creation of ordered book model and controller

---
+++ dunder_mifflin_book_club/app/controllers/books_controller.rb
@@ -0,0 +1,128 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  # GET /books
+  # GET /books.json
+  def index
+   if(params[:s] != nil)
+      session[:sort_mode] = params[:s]
+    else
+      session[:sort_mode] = "c"
+    end
+
+    if(session[:sort_mode] == "a")
+      @books = Book.all.order("author ASC")
+    else
+      @books = Book.all.order("sort_order DESC")
+    end
+  end
+
+  def move_up
+    resort_orders
+    @book = Book.find(params[:id])
+    @above_book = Book.all.where(sort_order: @book.sort_order + 1).first
+
+    if(@above_book != nil)
+      new_order = @above_book.sort_order
+      @above_book.sort_order = @book.sort_order
+      @book.sort_order = new_order
+      @above_book.save
+      @book.save
+    end
+
+    redirect_to "/books"
+  end
+
+  def move_down
+    resort_orders
+    @book = Book.find(params[:id])
+    @above_book = Book.all.where(sort_order: @book.sort_order - 1).first
+
+    if(@above_book != nil)
+      new_order = @above_book.sort_order
+      @above_book.sort_order = @book.sort_order
+      @book.sort_order = new_order
+      @above_book.save
+      @book.save
+    end
+
+    redirect_to "/books"
+  end
+
+  # GET /books/1
+  # GET /books/1.json
+  def show
+  end
+
+  # GET /books/new
+  def new
+    @book = Book.new
+  end
+
+  # GET /books/1/edit
+  def edit
+  end
+
+  # POST /books
+  # POST /books.json
+  def create
+    @book = Book.new(book_params)
+    resort_orders
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /books/1
+  # PATCH/PUT /books/1.json
+  def update
+    resort_orders
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /books/1
+  # DELETE /books/1.json
+  def destroy
+    @book.destroy
+    resort_orders
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def resort_orders
+      @books = Book.all.order("sort_order ASC")
+      i = 0
+      @books.each do |book|
+        book.sort_order = i
+        book.save
+        i = i+1
+      end
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def book_params
+      params.require(:book).permit(:title, :auhtor, :publish_date, :description)
+    end
+end
--- dunder_mifflin_book_club/config/routes.rb
+++ dunder_mifflin_book_club/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  	resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root 'books#index'
+   	get "books/:id/move_up" => 'books#move_up'
+   	get "books/:id/move_down" => 'books#move_down'
 end
